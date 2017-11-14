1605294fe2f335f8dfe273e9462a966e82b5997e
OrzOrc <qiucheng48@gmail.com>
2017-11-14 14:45:19 +0800

    添加默认配置位置

--- src/UpdateBase.php
+++ src/UpdateBase.php
@@ -9,18 +9,34 @@ abstract class UpdateBase
 {
     private $currentIP = '';
 
-    private $currentIPFile = '';
+    protected $currentIPFile = '';
+
+    protected static $configDir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config';
 
     const CURRENT_IP_URL = 'http://freegeoip.net/json/';
 
     public function __construct($config = [])
     {
+        $baseConfigFile = self::$configDir . DIRECTORY_SEPARATOR . 'base.php';
+        $baseConfig = require($baseConfigFile);
+
+        $className = get_class();
+        $instanceConfigFile = lcfirst(substr($className, strrpos($className, '\\') + 1)) . "php";
+        $instanceConfig = require(self::$configDir . DIRECTORY_SEPARATOR . $instanceConfigFile);
+
+        $config = array_merge($baseConfig, $instanceConfig, $config);
+
         foreach ($config as $key => $value) {
             $fun = 'set' . ucfirst($key);
             if (method_exists($this, $fun)) {
                 $this->$fun($value);
             }
         }
+    }
+
+    public static function setConfigDir($path)
+    {
+        self::$configDir = $path;
     }
 
     public function runUpdate()
